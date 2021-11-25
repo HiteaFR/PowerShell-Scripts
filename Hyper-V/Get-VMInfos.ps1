b380a60e5851dbbea4934c2918e8fbc6e070f0bf
David-Alexandre <9982795+DocKob@users.noreply.github.com>
2021-11-25 22:21:28 +0100

    Update Scripts

--- Hyper-V/Get-VMInfos.ps1
+++ Hyper-V/Get-VMInfos.ps1
@@ -3,27 +3,20 @@ Import-Module Hyper-V
 [securestring]$Password = ConvertTo-SecureString 'Password' -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PsCredential("User", $Password)
 
-$Hosts = @("Server")
-
 $ReportsPath = (Join-Path $env:LOCALAPPDATA "Hitea\AmInf\Reports")
 New-Item -Path $ReportsPath -ItemType Directory -Force | Out-Null
 
-$VmsState = [enum]::GetNames([Microsoft.HyperV.Powershell.VMState])
-$VmsStatus = [enum]::GetNames([Microsoft.HyperV.Powershell.VMOperationalStatus])
+$VMhost = Get-CimInstance -ClassName Win32_OperatingSystem -ErrorAction Stop
+$Vms = Get-VM -VMName * 
 
-$VMhost = Get-CimInstance -computername $Computername -ClassName Win32_OperatingSystem -ErrorAction Stop
-
-# $VM = Invoke-Command -ComputerName $Hosts -Credential $Credential -ScriptBlock { }
-$VM = Get-VM -VMName *
-
-$vmusage = $VM | Select-Object Name, State, OperationalStatus, Status,
+$VMsDetail = $Vms | Select-Object Id, Name, State, OperationalStatus, Status,
 @{Name = "MemoryStatus"; Expression = { $_.MemoryStatus } },
 @{Name = "MemAssignMB"; Expression = { $_.MemoryAssigned / 1MB } },
 @{Name = "PctAssignTotal"; Expression = { [math]::Round(($_.memoryAssigned / ($vmhost.TotalVisibleMemorySize * 1KB)) * 100, 2) } },
 @{Name = "MemDemandMB"; Expression = { $_.MemoryDemand / 1MB } },
 @{Name = "PctDemandTotal"; Expression = { [math]::Round(($_.memoryDemand / ($vmhost.TotalVisibleMemorySize * 1KB)) * 100, 2) } }
-     
-$Server = [pscustomobject]@{
+  
+$ServerDetail = [pscustomobject]@{
     Computername         = $vmhost.CSName
     OperatingSystem      = $vmhost.Caption
     TotalMemory          = $vmhost.totalVisibleMemorySize / 1MB -as [int]
@@ -36,96 +29,53 @@ $Server = [pscustomobject]@{
     TotalAssignedMemory  = ($vms | Measure-Object -Property MemoryAssigned -sum).sum / 1GB 
     TotalDemandMemory    = ($vms | measure-object -Property MemoryDemand -sum).sum / 1GB 
     PctDemand            = [math]::Round(($vms.memorydemand | measure-object -sum).sum / ($vms.memoryassigned | measure-object -sum).sum * 100, 2)
-    TotalMaximumMemory   = ($vms | Measure-Object -Property MemoryMaximum -sum).sum / 1GB 
-    VMs                  = $vmusage
+    TotalMaximumMemory   = ($vms | Measure-Object -Property MemoryMaximum -sum).sum / 1GB
 }
 
-foreach ($Item in $Server.VMs) {
-    $Item
-}
+$ServerDetail | Export-Csv -Path (Join-Path $ReportsPath "$($VMhost.CSName)_Infos.csv") -Encoding UTF8 -Delimiter ";" -NoTypeInformation
 
-$Server.VMs | Export-Csv -Path (Join-Path $ReportsPath "$($vmhost.CSName)_VMs.csv") -Encoding UTF8 -Delimiter ";" -NoTypeInformation
+$VMsDetail | Export-Csv -Path (Join-Path $ReportsPath "$($VMhost.CSName)_VMs.csv") -Encoding UTF8 -Delimiter ";" -NoTypeInformation
 
-Foreach ($Item in $VM) {
+Get-EventLog -Logname System -After (Get-Date).AddHours(-24) | Where-Object { $_.EventID -in (6005, 6006, 6008, 6009, 1074, 1076) }
 
-    $Computer_Id = $Item.Id
+Foreach ($Item in $VMsDetail) {
 
     if ($Item.State -eq "Running") {
 
-        Write-Host "Get Name of Item $Computer_Id"
-        try {
-            $remoteComputerName = Invoke-Command -VMId $Item.Id -Credential $Credential -ScriptBlock { $env:computername } -ErrorAction Stop
-            Write-Output $remoteComputerName
-            Invoke-Command -VMId $Item.Id -Credential $Credential -ScriptBlock { 
+        Invoke-Command -VMId $Item.Id -Credential $Credential -ScriptBlock {
 
-                $WinAppx = Get-AppPackage
-                $WinAppx
+            $VMInfos = Get-CimInstance -ClassName Win32_OperatingSystem
+            $Win32Apps = Get-WmiObject -Class Win32_Product
+            $WinAppx = Get-AppxPackage
 
-                $Win32AppObj = Get-WmiObject -Class Win32_Product
-                $Win32AppObj.properties
+            Write-Host $VMInfos.CSName
+            Write-Host $VMInfos.Caption
+            $VMInfos | Format-List
+            Get-WmiObject -Class Win32_Product | Format-Table
+            $WinAppx | Format-Table
 
-            } -ErrorAction Stop  
-        }
-        catch {
-            Write-Output "Fail"
-        }
-    
+        } -ErrorAction Stop
+
     }
 }
 
 <#
-
-PSComputerName         : Windows-10
-RunspaceId             : 9d089916-1433-4b86-bf2f-ef6200202a84
-Name                   : Microsoft.BingWeather
-Publisher              : CN=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=Washington, C=US
-PublisherId            : 8wekyb3d8bbwe
-Architecture           : X64
-ResourceId             :
-Version                : 4.25.20211.0
-PackageFamilyName      : Microsoft.BingWeather_8wekyb3d8bbwe
-PackageFullName        : Microsoft.BingWeather_4.25.20211.0_x64__8wekyb3d8bbwe
-InstallLocation        : C:\Program Files\WindowsApps\Microsoft.BingWeather_4.25.20211.0_x64__8wekyb3d8bbwe
-IsFramework            : False
-PackageUserInformation : {}
-IsResourcePackage      : False
-IsBundle               : False
-IsDevelopmentMode      : False
-NonRemovable           : False
-Dependencies           : { Microsoft.NET.Native.Framework.2.2_2.2.27405.0_x64__8wekyb3d8bbwe,
-    Microsoft.NET.Native.Runtime.2.2_2.2.27328.0_x64__8wekyb3d8bbwe,
-    Microsoft.Advertising.Xaml_10.1808.3.0_x64__8wekyb3d8bbwe,
-    Microsoft.VCLibs.140.00_14.0.27323.0_x64__8wekyb3d8bbwe... }
-IsPartiallyStaged      : False
-SignatureKind          : Store
-Status                 : Ok
-
-
-PSComputerName : Windows-10
-RunspaceId     : 9d089916-1433-4b86-bf2f-ef6200202a84
-Name           : InstallState
-Value          : 5
-Type           : SInt16
-IsLocal        : True
-IsArray        : False
-Origin         : Win32_Product
-Qualifiers     : { System.Management.QualifierData }
-
-PSComputerName : Windows-10
-RunspaceId     : 9d089916-1433-4b86-bf2f-ef6200202a84
-Name           : Language
-Value          : 0
-Type           : String
-IsLocal        : True
-IsArray        : False
-Origin         : Win32_Product
-Qualifiers     : { System.Management.QualifierData }
-
 
 Get-VM –Name * -Computername SRV01 | enable-vmresourcemetering
 
 Measure-VM -name * -Computername SRV01
 
 Get-VM  -VMName * -ComputerName SRV01 | Reset-VMResourceMetering
+
+$VmsState = [enum]::GetNames([Microsoft.HyperV.Powershell.VMState])
+$VmsStatus = [enum]::GetNames([Microsoft.HyperV.Powershell.VMOperationalStatus])
+
+        try {
+            Write-Host "Get infos of VM : $($Item.Name)"
+
+        }
+        catch {
+            Write-Output "Get VM infos failed"
+        }
 
 #>
