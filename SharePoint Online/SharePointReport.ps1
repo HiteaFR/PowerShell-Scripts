fd4c8d3ee13d7455a685fb8879005b553bb40240
David-Alexandre <68540705+HiteaFR@users.noreply.github.com>
2022-01-19 22:35:36 +0100

    Update Script - SP Report

--- SharePoint Online/SharePointReport.ps1
+++ SharePoint Online/SharePointReport.ps1
@@ -1,5 +1,5 @@
 #Groupe de sécurité
-
+<##
 $creduser = Read-Host "Admin email"
 $credpassword = Read-Host "Admin Password"
 
@@ -36,12 +36,25 @@ Foreach ($Group in $Groups) {
 
 $table | Export-Csv -Path "Groupe-SharePoint.csv" -Delimiter ";" -Encoding UTF8 -NoTypeInformation
 
+New-HTML {
+    New-HTMLTable -DataTable $table2 -Title 'SharePoint Library Rights' -HideFooter -PagingLength 25 -AlphabetSearch {
+        New-TableAlphabetSearch -ColumnName 'Name'
+    }
+} -ShowHTML -FilePath "SharePointReport.html" -Online
+
+#>
 
 #Dossier de la bibliothèque
 
-$SiteURL = "Site library URL"
-$ListName = "List Name"
+$creduser = Read-Host "Admin email"
+$credpassword = Read-Host "Admin Password"
+$SiteURL = Read-Host "Site library URL"
+$ListName = Read-Host "List Name"
 
+[securestring]$secStringPassword = ConvertTo-SecureString $credpassword -AsPlainText -Force
+[pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($creduser, $secStringPassword)
+
+Connect-AzureAD -Credential $credObject
 Connect-PnPOnline -Url $SiteURL -UseWebLogin
 
 $Table2 = New-Object 'System.Collections.Generic.List[System.Object]'
@@ -58,13 +71,38 @@ foreach ($Folder in $SpFolderList) {
     if ($file.ListItemAllFields.HasUniqueRoleAssignments -eq $True) {
         foreach ($roleAssignments in $file.ListItemAllFields.RoleAssignments) {
             Get-PnPProperty -ClientObject $roleAssignments -Property RoleDefinitionBindings, Member
+            $PermissionType = $roleAssignments.Member.PrincipalType
             $PermissionLevels = $roleAssignments.RoleDefinitionBindings | Select-Object -ExpandProperty Name
             $PermissionLevels = ($PermissionLevels | Where-Object { $_ -notlike "Acc*s limit*" }) -join ", "
             If ($PermissionLevels.Length -eq 0) { Continue }
-            $obj2 = [PSCustomObject]@{
-                'Name'  = $Folder.Name
-                'Group' = $roleAssignments.Member.Title
-                'Droit' = $PermissionLevels
+            if ($PermissionType -eq "SecurityGroup") {
+
+                $Group = Get-AzureAdGroup -Filter "DisplayName eq '$($roleAssignments.Member.Title)'" | Where-Object { $_.MailEnabled -eq $false -and $_.SecurityEnabled -eq $true }
+                
+                if (!$Group) {
+                    $Users = "Groupe Introuvable: Erreur"
+                }
+                else {
+                    $Users = (Get-AzureADGroupMember -ObjectId $Group.ObjectID | Sort-Object DisplayName | Select-Object -ExpandProperty DisplayName) -join ", "
+                }
+
+
+                $obj2 = [PSCustomObject]@{
+                    'Dossier' = $Folder.Name
+                    'Type'    = $PermissionType
+                    'Nom'     = $roleAssignments.Member.Title
+                    'Membres' = $Users
+                    'Droit'   = $PermissionLevels
+                }
+            }
+            else {
+                $obj2 = [PSCustomObject]@{
+                    'Dossier' = $Folder.Name
+                    'Type'    = $PermissionType
+                    'Nom'     = $roleAssignments.Member.Title
+                    'Membres' = ""
+                    'Droit'   = $PermissionLevels
+                }
             }
             $table2.add($obj2)
         }
@@ -73,9 +111,3 @@ foreach ($Folder in $SpFolderList) {
 }
 
 $table2 | Export-Csv -Path "Dossier-SharePoint.csv" -Delimiter ";" -Encoding UTF8 -NoTypeInformation
-
-New-HTML {
-    New-HTMLTable -DataTable $table2 -Title 'SharePoint Library Rights' -HideFooter -PagingLength 25 -AlphabetSearch {
-        New-TableAlphabetSearch -ColumnName 'Name'
-    }
-} -ShowHTML -FilePath "SharePointReport.html" -Online
