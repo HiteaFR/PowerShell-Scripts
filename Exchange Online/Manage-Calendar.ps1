eca028259c95c5438c7ba73d558795cdd0a86588
David-Alexandre <68540705+HiteaFR@users.noreply.github.com>
2022-02-09 08:03:31 +0100

    Update Script - Calendars

--- Exchange Online/Manage-Calendar.ps1
+++ Exchange Online/Manage-Calendar.ps1
@@ -1,1 +1,51 @@
-Get-Mailbox -ResultSize Unlimited | ForEach { Get-MailboxFolderPermission -Identity "$($_.PrimarySMTPAddress):\Calendrier" } | Select Identity, User, AccessRights
+$Compte = Read-Host "Choisir un compte (user@domaine.fr)"
+$Action = Read-Host "V pour voir ou M pour voir et modifier"
+
+$AuditMailboxe = Get-Mailbox -Identity $Compte
+$Mailboxes = Get-Mailbox -ResultSize Unlimited | Where-Object RecipientTypeDetails -eq "UserMailbox" | Where-Object Identity -notlike "*Meeting4Display*" | Where-Object Identity -notlike "*notification*"
+
+foreach ($Bal in $Mailboxes) {
+
+    if ($BAL.Languages -like "*FR*") {
+        $Calendar = Get-MailboxFolderPermission -Identity "$($BAL.PrimarySMTPAddress):\Calendrier" -ErrorAction SilentlyContinue | Select Identity, User, AccessRights
+    }
+    else {
+        $Calendar = Get-MailboxFolderPermission -Identity "$($BAL.PrimarySMTPAddress):\Calendar" -ErrorAction SilentlyContinue | Select Identity, User, AccessRights
+    }
+
+    if ($Calendar.User.DisplayName -notcontains $AuditMailboxe.Identity) {
+        Write-Host "$($AuditMailboxe.Identity) n'a pas acces au Calendrier de $($Bal.Identity)" -ForegroundColor Yellow
+        if ($Action -eq "M") {
+
+            if ($BAL.Languages -like "*FR*") {
+
+                Add-MailboxFolderPermission -Identity ($Bal.Identity + ":\Calendrier") -User $AuditMailboxe.UserPrincipalName -AccessRights Reviewer
+
+            }
+            else {
+
+                Add-MailboxFolderPermission -Identity ($Bal.Identity + ":\Calendar") -User $AuditMailboxe.UserPrincipalName -AccessRights Reviewer
+            
+            }
+        }
+
+    }
+    else {
+        Write-Host "$($AuditMailboxe.Identity) a acces au Calendrier de $($Bal.Identity)" -ForegroundColor Green
+    }
+
+}
+
+<#
+
+$valuesToLookFor = @(
+    'Completed',
+    'CompletedWithErrors',
+    'Corrupted',
+    'Failed',
+    'Stopped')
+
+(Get-Migrationbatch -Identity $MigrationBatchName |
+    Where-Object { $valuesToLookFor -contains $_.Status })
+
+#>
